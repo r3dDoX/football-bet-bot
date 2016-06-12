30be2cf477da5790e27078d1c62e19aebbda2541
r3dDoX <patrick@walther-online.ch>
2016-06-12 13:23:07 +0200

    refactor to more concise version and add better logging

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cheerio = require('cheerio');
 const team1 = process.argv[2];
 const team2 = process.argv[3];
 
+const iterations = 100000;
 const correction = 10;// to get even numbers throughout calculation (js numbers...)
 const goalQuantifier = 0.0045 * correction;
 const typicalGoals = 2.6 * correction;
+const goalTriesQuantifier = 6;
 
 const teams = [
     'Albania',
@@ -43,55 +45,53 @@ function getCurrentEloRatings() {
         }, (response) => {
             let body = '';
 
-            response.on('data', (d) => body += d);
-
+            response.on('data', data => body += data);
             response.on('end', () => resolve(body));
-            
-            response.on('error', () => reject());
+            response.on('error', error => reject(error));
+            response.on('timeout', error => reject(error));
         });
     });
 }
 
 function getAverageStrength($) {
-    return Math.round(teams
-        .map(teamName => Number($(`table a:contains(${teamName})`).parents('tr').find('td:nth-child(4)').html()))
-        .reduce((sum, teamStrength) => sum + teamStrength, 0) / teams.length);
+    return teams
+            .map(teamName => Number($(`table a:contains(${teamName})`).parents('tr').find('td:nth-child(4)').html()))
+            .reduce((sum, teamStrength) => sum + teamStrength, 0) / teams.length;
 }
 
 getCurrentEloRatings().then(data => {
     const $ = cheerio.load(data);
-    
-    const teamRow1 = $(`table a:contains(${team1})`).parents('tr');
-    const teamRow2 = $(`table a:contains(${team2})`).parents('tr');
-    const eloRating1 = Number($('td:nth-child(4)', teamRow1).html());
-    const eloRating2 = Number($('td:nth-child(4)', teamRow2).html());
 
-    const averageStrength = getAverageStrength($);
+    const eloRating1 = Number($('td:nth-child(4)', $(`table a:contains(${team1})`).parents('tr')).html());
+    const eloRating2 = Number($('td:nth-child(4)', $(`table a:contains(${team2})`).parents('tr')).html());
 
-    const teamStrength1 = eloRating1 - averageStrength;
-    const teamStrength2 = eloRating2 - averageStrength;
+    if (eloRating1.length === 0) {
+        throw new Error(`Couldn't find ELO Rating for team ${team1}`);
+    }
+    if (eloRating2.length === 0) {
+        throw new Error(`Couldn't find ELO Rating for team ${team2}`);
+    }
 
-    const goalDifference1 = Math.round(teamStrength1 * goalQuantifier);
-    const goalDifference2 = Math.round(teamStrength2 * goalQuantifier);
+    const averageStrength = Math.round(getAverageStrength($));
+    const averageGoals1 = Math.round((eloRating1 - averageStrength) * goalQuantifier);
+    const averageGoals2 = Math.round((eloRating2 - averageStrength) * goalQuantifier);
 
-    const goalDifference = goalDifference1 - goalDifference2;
-    const likelyGoals2 = (typicalGoals - goalDifference)/2;
-    const likelyGoals1 = typicalGoals - likelyGoals2;
+    const typicalGoalShare2 = (typicalGoals - (averageGoals1 - averageGoals2)) / 2;
+    const typicalGoalShare1 = typicalGoals - typicalGoalShare2;
 
-    const dice1 = Array.apply(null, Array(Math.ceil((likelyGoals1 * 6)/correction)));
-    const dice2 = Array.apply(null, Array(Math.ceil((likelyGoals2 * 6)/correction)));
+    const goalTries1 = Array.apply(null, Array(Math.ceil((typicalGoalShare1 * goalTriesQuantifier) / correction)));
+    const goalTries2 = Array.apply(null, Array(Math.ceil((typicalGoalShare2 * goalTriesQuantifier) / correction)));
 
-    const iterations = 100000;
     const calculateGoals = (triesArray) => {
         return triesArray
             .map(() => Math.ceil(Math.random() * 6))
-            .reduce((sum, result) => result === 6 ? sum + 1 : sum, 0);
+            .reduce((sum, actTryResult) => actTryResult === 6 ? sum + 1 : sum, 0);
     };
     const summedResults = Array.apply(null, Array(iterations))
         .map(() => {
             return {
-                result1: calculateGoals(dice1),
-                result2: calculateGoals(dice2)
+                result1: calculateGoals(goalTries1),
+                result2: calculateGoals(goalTries2)
             };
         })
         .reduce((sum, actResult) => {
@@ -100,20 +100,20 @@ getCurrentEloRatings().then(data => {
                 sum[index].count += 1;
             } else {
                 sum[index] = {
-                    result: actResult.result1 + ' - ' + actResult.result2,
+                    result: `${actResult.result1} - ${actResult.result2}`,
                     count: 1
                 };
             }
             return sum;
         }, {});
 
-    const bestResult = Object.getOwnPropertyNames(summedResults).reduce((bestResult, actPropertyName) => {
-        const actResult = summedResults[actPropertyName];
-        if (bestResult.count < actResult.count) {
-            return actResult;
-        }
-        return bestResult;
-    }, {count:0});
-
-    console.log(bestResult);
-});
+    return Object.getOwnPropertyNames(summedResults).reduce((bestResult, actResultIndex) => {
+        const actResult = summedResults[actResultIndex];
+        return (bestResult.count <= actResult.count) ? actResult : bestResult;
+    }, {count: 0});
+})
+    .then(bestResult => {
+        console.log(`The most likely result is: ${bestResult.result}.`);
+        console.log(`Out of ${iterations} tries, it happened ${bestResult.count} times`);
+    })
+    .catch(error => console.error(error));
